e636201c3b0c3eb633331cea4a12f65f839f0419
Lila Mullany <lilamullany@gmail.com>
2020-08-13 12:09:48 -0700

    refactored to take in directory or individual file

--- plot_validation.py
+++ plot_validation.py
@@ -1,21 +1,31 @@
 import matplotlib.pyplot as plt
 import glob
- 
+import sys
+import os
+
 
 def main():
-    files_to_parse = [
-        "version2_output/tensorflow-logs.log",
-        "version2_output/tensorflow-logs2.log"
-    ]
-    
+    files_to_parse = get_files(sys.argv[1])
+
     parser = Parser(files_to_parse, [], [])
     parser.update_points()
-    
+
     plt.xlabel('Steps')
     plt.ylabel('Loss')
     plt.title("Refuse Model Version 2 Loss vs. Steps")
     plt.plot(parser.steps, parser.loss)
     plt.show()
+
+
+def get_files(f):
+    files_to_parse = []
+    if os.path.isdir(f):
+        for file in os.listdir(f):
+            files_to_parse.append(os.path.join(f, file))
+    else:
+        files_to_parse.append(f)
+    return files_to_parse
+
 
 class Parser:
     def __init__(self, files_to_parse, steps=[], loss=[]):
